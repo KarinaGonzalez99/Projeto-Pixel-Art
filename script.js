38270e41435c48669ece117785de8d726f61f388
Kaka <kaahgonzalez99@gmail.com>
2022-10-05 14:28:19 -0300

    erro no cypress denovo

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById('button-random-color');
+let coresAleatorias = document.getElementsByClassName('color');
+let elementoCompleto = document.getElementsByTagName('body')[0]
+
+function gerador() {
+    button.addEventListener('click', function () { // function dentro pois é anônima, poderia ficar dentro qnto fora, ou usar
+        for (let i = 1; i < coresAleatorias.length; i += 1) {
+            let corAleatoria = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+            let corAtual = coresAleatorias[i]
+            corAtual.style.backgroundColor = corAleatoria;
+        }
+    })
+}
+gerador();
+
+
