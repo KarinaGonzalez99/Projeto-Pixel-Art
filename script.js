96a6cec4c4ef2a4d856a5a0b8942b04a21f2d3b9
Kaka <kaahgonzalez99@gmail.com>
2022-10-05 01:05:21 -0300

    testando requisito 4, erro no crypress

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let coresAleatorias = document.getElementsByClassName('color');
 let elementoCompleto = document.getElementsByTagName('body')[0]
 
 function gerador() {
-    button.addEventListener('click', function () {
+    button.addEventListener('click', function () { // function dentro pois é anônima, poderia ficar dentro qnto fora, ou usar
         for (let i = 1; i < coresAleatorias.length; i += 1) {
             let corAleatoria = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
             let corAtual = coresAleatorias[i]
@@ -12,3 +12,8 @@ function gerador() {
     })
 }
 gerador();
+
+localStorage.setItem('color2');
+localStorage.setItem('color3');
+localStorage.setItem('color4');
+
