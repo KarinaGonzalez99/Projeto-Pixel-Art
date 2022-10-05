593404521294fc9cd8d1299578b65f5d95de6ac4
Kaka <kaahgonzalez99@gmail.com>
2022-10-05 16:40:52 -0300

    editando denovo

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('button-random-color');
+let coresAleatorias = document.getElementsByClassName('color');
+
+function gerador() {
+    button.addEventListener('click', function () { // function dentro pois é anônima, poderia ficar dentro qnto fora, ou usar
+        for (let i = 1; i < coresAleatorias.length; i += 1) {
+            let quaseerrei = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+            let corAtual = coresAleatorias[i]
+            corAtual.style.backgroundColor = quaseerrei;
+        }
+    })
+}
+gerador();
